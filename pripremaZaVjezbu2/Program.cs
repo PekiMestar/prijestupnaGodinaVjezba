234637b78c46d59b5eeb7449ebe6010df806e038
PekiMestar <145533879+PekiMestar@users.noreply.github.com>
2023-11-08 09:01:06 +0100

    Add files via upload

---
+++ pripremaZaVjezbu2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pripremaZaVjezbu2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1900; i <= 2100; i++)
+            {
+                if(i % 4 == 0 && i % 100 != 0 || i % 400 == 0 )
+                {
+                    Console.WriteLine(i + " je prijestupna godina.");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
